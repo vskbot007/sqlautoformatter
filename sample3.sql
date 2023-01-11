a214367ec40edc4d52fbb75091d91c08b7ae1953
V Sai kumar <80595392+vskbot007@users.noreply.github.com>
2023-01-11 11:56:01 +0530

    Update sample3.sql

--- sample3.sql
+++ sample3.sql
@@ -1,6 +1,16 @@
 SELECT
   call.*,
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   DATEDIFF("SECOND", call.start_time, call.end_time) AS call_duration
-FROM
+                           FROM
   call
 ORDER BY call.employee_id, call.start_time;
