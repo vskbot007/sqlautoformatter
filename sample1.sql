4b49de27ccbe48bcdea4a9080b2bd4cebc7c4418
V Sai kumar <80595392+vskbot007@users.noreply.github.com>
2023-01-12 12:23:17 +0530

    Create sample1.sql

---
+++ sample1.sql
@@ -0,0 +1,7 @@
+SELECT 
+    call.*,
+                DATEDIFF("SECOND", call.start_time, call.end_time) AS call_duration
+FROM call
+            ORDER BY
+    call.employee_id ASC,
+    call.start_time ASC;
