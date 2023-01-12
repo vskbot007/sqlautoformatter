870993819e9a1c662bf54a6d9e79f214244a4a27
V Sai kumar <80595392+vskbot007@users.noreply.github.com>
2023-01-12 17:28:06 +0530

    Update sample9.sql

--- sample9.sql
+++ sample9.sql
@@ -1,6 +1,4 @@
-SELECT emp_id, first_name,last_name,dept_id,mgr_id,
-         WIDTH_BUCKET(department_id,20,40,10) "Exists in Dept"
-FROM emp
-    
-    WHERE mgr_id < 300
-ORDER BY "Exists in Dept";
+INSERT INTO Yearly_Orders 
+         SELECT * FROM Orders 
+      
+      WHERE Date<=1/1/2018
