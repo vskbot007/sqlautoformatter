ec8c47a07513007f626cc2d1347d2785fd1ea815
V Sai kumar <80595392+vskbot007@users.noreply.github.com>
2023-01-18 12:41:02 +0530

    Update sample9.sql

--- sample9.sql
+++ sample9.sql
@@ -1,6 +1,25 @@
 CREATE TABLE Customers
-(
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      (
+         
+  
+  
+  
+  
+  
+  
+  
+  
+  
   ID int NOT NULL,
-  Name varchar(80) NOT NULL,
+                                Name varchar(80) NOT NULL,
   PRIMARY KEY(ID)
 );
