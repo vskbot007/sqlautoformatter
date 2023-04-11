585914cce59acb49fb6b5b3044470433e4b1b338
V Sai kumar <80595392+vskbot007@users.noreply.github.com>
2023-04-11 15:47:12 +0530

    Create sam.sql

---
+++ sam.sql
@@ -0,0 +1,16 @@
+UPDATE dbVersion
+SET
+  version = '0.6.9';
+GO
+
+
+
+
+ALTER TABLE user ADD CONSTRAINT FK_user_parentIdTag FOREIGN KEY(parentIdTag) 
+
+
+
+
+
+
+REFERENCES user(idTag) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION ENFORCED;
